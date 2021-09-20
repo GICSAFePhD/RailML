2658cc221b05d2ca928aa1f5804ec77da146a809
Martin-N-Menendez <menendez91@live.com.ar>
2021-09-20 20:53:03 -0300

    Starting save_XML

--- Infrastructure/NetRelation.py
+++ Infrastructure/NetRelation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+from RailML.RailTopoModel import RTM_Relation
 from RailML.Common import tElementWithIDref
 from RailML.Infrastructure import tNavigability, tUsage
 from RailML.RailTopoModel import RTM_NetworkResource
 from typing import List
 
-class NetRelation(RTM_NetworkResource.RTM_NetworkResource):
+#class NetRelation(RTM_NetworkResource.RTM_NetworkResource):
+class NetRelation(RTM_Relation.RTM_Relation):
 	"""The NetRelation type is derived from the RailTopoModel class PositionedRelation."""    
 	def __init__(self):
-		pass
+		super().__init__()
