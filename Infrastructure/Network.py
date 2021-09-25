f4a5a1b80a315e1bb186192875f1b0c67e1b8bab
Martin-N-Menendez <menendez91@live.com.ar>
2021-09-24 21:11:58 -0300

    Switches - Length error

--- Infrastructure/Network.py
+++ Infrastructure/Network.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-from RailML.RailTopoModel import RTM_Network,RTM_LevelNetwork
+from RailML.RailTopoModel import RTM_Network
 from typing import List
 
 class Network(RTM_Network.RTM_Network):
 	"""The Network type is derived from the RailTopoModel class Network."""
-	@property
-	def Level(self) -> RTM_LevelNetwork:
-		return self.___level	
-
-	@Level.setter
-	def Level(self, aLevel : RTM_LevelNetwork):
-		self.___level = aLevel
-
-	def create_Level(self):
-		if self.Level == None:
-			self.Level = []
-		self.Level.append(RTM_LevelNetwork.RTM_LevelNetwork())
 
 	def __init__(self):	
 		super().__init__()
-		self.___level : RTM_LevelNetwork = None
+		#self.___level : RTM_LevelNetwork = None
 		# @AssociationType Infrastructure.RTM.RTM_LevelNetwork*
 		# @AssociationMultiplicity 1..*
--- RailTopoModel/RTM_Network.py
+++ RailTopoModel/RTM_Network.py
@@ -7,33 +7,17 @@ from typing import List
 
 class RTM_Network(RTM_NamedResource.RTM_NamedResource):
 	@property
-	def Id(self) -> str:
-		return self.___id
-	@property
 	def Level(self) -> RTM_LevelNetwork:
 		return self.___level
-	@property
-	def NetworkResource(self) -> RTM_NetworkResource:
-		return self.___networkResource
 
-	@Id.setter
-	def Id(self, aId : str):
-		self.___id = aId
 	@Level.setter
 	def Level(self, aLevel : RTM_LevelNetwork):
 		self.___level = aLevel
-	@NetworkResource.setter
-	def NetworkResource(self, aNetworkResource : RTM_NetworkResource):
-		self.___networkResource = aNetworkResource
 	
 	def create_Level(self):
 		if self.Level == None:
 			self.Level = []
 		self.Level.append(RTM_LevelNetwork.RTM_LevelNetwork())
-	def create_NetworkResource(self):
-		if self.NetworkResource == None:
-			self.NetworkResource = []
-		self.NetworkResource.append(RTM_NetworkResource.RTM_NetworkResource())
 
 	def __init__(self):
 		super().__init__()
@@ -41,6 +25,6 @@ class RTM_Network(RTM_NamedResource.RTM_NamedResource):
 		self.___level : RTM_LevelNetwork = None
 		# @AssociationType Infrastructure.RTM.RTM_LevelNetwork*
 		# @AssociationMultiplicity 1..*
-		self.___networkResource : RTM_NetworkResource = None
+		#self.___networkResource : RTM_NetworkResource = None
 		# @AssociationType Infrastructure.RTM.RTM_NetworkResource*
 		# @AssociationMultiplicity 0..*
