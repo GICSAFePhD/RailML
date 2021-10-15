0ea2afc68f831bb12d3f6b69895970c115e5200d
Martin-N-Menendez <menendez91@live.com.ar>
2021-10-15 11:24:15 -0300

    Some attributes fixed

--- RailTopoModel/RTM_AreaLocation.py
+++ RailTopoModel/RTM_AreaLocation.py
@@ -3,10 +3,22 @@
 from RailML.RailTopoModel import RTM_AssociatedNetElement, RTM_EntityLocation
 from typing import List
 
-#class RTM_AreaLocation(RTM_EntityLocation): #TODO CON ESTA HERENCIA SE ROMPE
-class RTM_AreaLocation():
+class RTM_AreaLocation(RTM_EntityLocation.RTM_EntityLocation):
+	@property
+	def AssociatedNetElement(self) -> RTM_AssociatedNetElement:
+		return self.___associatedNetElement
+
+	@AssociatedNetElement.setter
+	def AssociatedNetElement(self, aAssociatedNetElement : RTM_AssociatedNetElement):
+		self.___associatedNetElement = aAssociatedNetElement
+    
+	def create_AssociatedNetElement(self):
+		if self.AssociatedNetElement == None:
+			self.AssociatedNetElement = []
+		self.AssociatedNetElement.append(RTM_AssociatedNetElement.RTM_AssociatedNetElement())
+
 	def __init__(self):
-		self._associatedNetElement : RTM_AssociatedNetElement = None
+		super().__init__()
+		self.___associatedNetElement : RTM_AssociatedNetElement = None
 		# @AssociationType Infrastructure.RTM.RTM_AssociatedNetElement*
 		# @AssociationMultiplicity 1..*
-
